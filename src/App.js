1476479e3e16d2b58fdbaa97f20a1d343c92a6cc
GeonI Kim <rlarjsdl9410@naver.com>
2024-12-24 11:28:16 +0900

    First Frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import MainPage from './components/MainPage/MainPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
